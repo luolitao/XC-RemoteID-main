df6235811fd6f4bec73ca54accf8176a7b5bc773
Eric Luo <luolitao@gmail.com>
2026-06-09 21:40:15 +0800

    cleanup: remove generated mavlink headers

---
+++ src/version_auto.h
@@ -0,0 +1,10 @@
+#pragma once
+// ========================================================
+// Auto-generated file by pre_build_version.py. 
+// DO NOT EDIT MANUALLY.
+// ========================================================
+#define AUTO_BUILD_YEAR  2026
+#define AUTO_BUILD_MONTH 6
+#define AUTO_BUILD_DAY   9
+#define AUTO_BUILD_HOUR  21
+#define AUTO_BUILD_MIN   35
